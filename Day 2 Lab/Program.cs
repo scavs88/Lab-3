f59a68c666bfccc5bc464a0533562104c17c1c72
sean <spcavanaugh88@gmail.com>
2021-04-06 17:06:37 -0400

    updated code

--- Day 2 Lab/Program.cs
+++ Day 2 Lab/Program.cs
@@ -12,30 +12,36 @@ namespace Day_2_Lab
             {
                 Console.WriteLine("What is your name?");//collect user name
                 string name = Console.ReadLine();
+                while (true)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 100");//collect number input
+                    int choice = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Please enter a number between 1 and 100");//collect number input
-                int choice = int.Parse(Console.ReadLine());
-
-                if (choice % 2 != 0)//choice is odd
-                {
-                    Console.WriteLine($"{name} {choice} is odd");
-                }
-                else if (choice % 2 == 0 && choice >= 2 && choice <= 25)//choice is even and less than 25
-                {
-                    Console.WriteLine($"{name} {choice} is even and less than 25");
-                }
-                else if (choice % 2 == 0 && choice >= 26 && choice <= 60)//choice is even and between 25 and 60
-                {
-                    Console.WriteLine($"{name} is even");
-                }
-                else if (choice % 2 == 0 && choice >= 60 && choice < 100)//choice is even and greater than 60
-                {
-                    Console.WriteLine($"{name} {choice} is even");
-                }
-                else if (choice <= 0 || choice > 100)
-                {
-                    Console.WriteLine("Invalid input.  Please select a number between 1 and 100");
-                    continue;
+                    if (choice % 2 != 0 && choice <= 100)//choice is odd
+                    {
+                        Console.WriteLine($"{name} {choice} is odd");
+                        break;
+                    }
+                    else if (choice % 2 == 0 && choice >= 2 && choice <= 25)//choice is even and less than 25
+                    {
+                        Console.WriteLine($"{name} {choice} is even and less than 25");
+                        break;
+                    }
+                    else if (choice % 2 == 0 && choice >= 26 && choice <= 60)//choice is even and between 25 and 60
+                    {
+                        Console.WriteLine($"{name} is even");
+                        break;
+                    }
+                    else if (choice % 2 == 0 && choice >= 60 && choice < 100)//choice is even and greater than 60
+                    {
+                        Console.WriteLine($"{name} {choice} is even");
+                        break;
+                    }
+                    else if (choice <= 0 || choice > 100)
+                    {
+                        Console.WriteLine("Invalid input.  Please select a number between 1 and 100");
+                        continue;
+                    }
                 }
                 Console.WriteLine("Would you like to enter another number? y/n");
                 ending = Console.ReadLine();
